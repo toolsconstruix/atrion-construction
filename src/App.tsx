1a6da24d8267ee331ac94f80c24e644733b8f2c8
wagnerfive <wfss1982@gmail.com>
2025-11-21 19:16:25 -0300

    Implement cleaningservices redirect in React Router instead of vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
 import Index from "./pages/Index";
 import Bathroom from "./pages/Bathroom";
 import Kitchen from "./pages/Kitchen";
@@ -11,6 +12,13 @@ import RenovationRemodeling from "./pages/RenovationRemodeling";
 import ResidentialConstruction from "./pages/ResidentialConstruction";
 import CommercialConstruction from "./pages/CommercialConstruction";
 import NotFound from "./pages/NotFound";
+
+const CleaningServicesRedirect = () => {
+  useEffect(() => {
+    window.location.href = "https://cleaningservices.atrionconstruction.com/";
+  }, []);
+  return null;
+};
 
 const queryClient = new QueryClient();
 
@@ -22,6 +30,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/cleaningservices/*" element={<CleaningServicesRedirect />} />
           <Route path="/services/bathroom" element={<Bathroom />} />
           <Route path="/services/kitchen" element={<Kitchen />} />
           <Route path="/services/painting" element={<Painting />} />
